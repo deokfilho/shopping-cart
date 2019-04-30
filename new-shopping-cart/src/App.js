1e99677c811ab68d3165b94f285b1c2235fefff8
deokfilho <deokcelmofilho@gmail.com>
2019-04-30 18:22:52 -0500

    can display json string locally

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [data, setData] = useState('null');
+   useEffect(() => {
+       const fetchJSON = async () => {
+           const response = await fetch('/data/products.json');
+           if (!response.ok) throw response;
+            const json = await response.json();
+           setData(json);
+           // setSchedule(json);
+       };
+       fetchJSON();
+   }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+      <pre>
+      {JSON.stringify(data, null, 2)}
+      </pre>
+      </div>
   );
 }
 
 export default App;
+// <div className="App">
+//   <header className="App-header">
+//     <img src={logo} className="App-logo" alt="logo" />
+//     <p>
+//       Edit <code>src/App.js</code> and save to reload.
+//     </p>
+//     <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//     >
+//       Learn React
+//     </a>
+{/*  </header>*/}
+{/*</div>*/}
